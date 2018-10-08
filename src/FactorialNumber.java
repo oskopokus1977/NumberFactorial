0817ca49e0650adaaefbfd8e34288a3266989275
Yurii Kozlov <oskopokus1977@gmail.com>
2018-10-08 18:37:56 +0300

    added factorial-number for bid values

--- src/FactorialNumber.java
+++ src/FactorialNumber.java
@@ -1,11 +1,18 @@
+import java.math.BigInteger;
+
 public class FactorialNumber {
 
   public static void main(String[] args) {
+    //  factorial number calculation with big integer
+    BigInteger bigInteger = new BigInteger("5040");
+    System.out.println(factorialBigNumber(bigInteger));
 
-    System.out.println(factorialNumber(720));
+    //  factorial number calculation with integer
+    System.out.println(factorialNumber(5040));
   }
 
-  public static int factorialNumber(int factorial) {
+
+  public static int factorialNumber(long factorial) {
     if (factorial <= 1) {
       return 1;
     }
@@ -20,4 +27,26 @@ public class FactorialNumber {
     }
     return n - 1;
   }
+
+
+  public static BigInteger factorialBigNumber(BigInteger factorial) {
+    if (factorial.equals(BigInteger.ONE)) {
+      return BigInteger.ONE;
+    }
+    BigInteger oneOper = BigInteger.ONE;
+    while (factorial.compareTo(BigInteger.ONE) != 0) {
+
+      if (factorial.mod(oneOper).compareTo(BigInteger.ZERO) == 0) {
+        factorial = factorial.divide(oneOper);
+        oneOper = oneOper.add(BigInteger.ONE);
+      } else {
+        System.out.println("This number is not factorial for any number");
+        oneOper = BigInteger.ONE;
+        break;
+      }
+    }
+    return oneOper.subtract(BigInteger.ONE);
+  }
+
+
 }
